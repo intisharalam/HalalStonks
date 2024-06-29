2bd52f98ca6f864121aeab46d0ff2951e5318aeb
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-29 21:57:23 +0100

    tried fix error on vercel

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:8000/api/:path*' // Local development URL for Django
+          : 'https://halal-stonks.vercel.app/api/:path*', // Replace with your actual Django backend URL on Vercel
+      },
+      {
+        source: '/',
+        destination: '/home', // Example destination for your Next.js routes
+      },
+    ];
+  },
+};
+
+export default nextConfig;
